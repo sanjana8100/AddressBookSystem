74f81c74fb3549feec91144a73934ca32c53bf20
sanjana8100 <sanjanams812@gmail.com>
2023-03-09 12:50:03 +0530

    added: UC1

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    public String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Scanner in= new Scanner(System.in);
+        AddressBookMain contact= new AddressBookMain();
+        System.out.println("Enter the following details to add the Contact to the Address Book:");
+        System.out.print("First Name: ");
+        contact.firstName= in.nextLine();
+        System.out.print("Last Name: ");
+        contact.lastName= in.nextLine();
+        System.out.print("Address: ");
+        contact.address= in.nextLine();
+        System.out.print("City: ");
+        contact.city= in.nextLine();
+        System.out.print("State: ");
+        contact.state= in.nextLine();
+        System.out.print("ZIP Code: ");
+        contact.zipCode= in.nextLine();
+        System.out.print("Phone Number: ");
+        contact.phoneNumber= in.nextLine();
+        System.out.print("E-mail Address: ");
+        contact.email= in.nextLine();
+
+        System.out.println("____________________________________________________");
+        System.out.println("The Contact Details:");
+        System.out.println("Name: "+contact.firstName+" "+contact.lastName);
+        System.out.println("Address: "+contact.address+"\n"+contact.city+", "+contact.state+" - "+ contact.zipCode);
+        System.out.println("Phone Number: "+contact.phoneNumber+"\nE-mail Address: "+contact.address);
     }
 }
