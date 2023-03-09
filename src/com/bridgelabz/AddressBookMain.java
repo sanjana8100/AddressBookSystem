671f2fe610178a81d9311a0cea8784eaa7c7c2f9
sanjana8100 <sanjanams812@gmail.com>
2023-03-09 13:31:13 +0530

    added: UC2

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,33 +3,60 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Scanner in= new Scanner(System.in);
-        AddressBookMain contact= new AddressBookMain();
-        System.out.println("Enter the following details to add the Contact to the Address Book:");
-        System.out.print("First Name: ");
-        contact.firstName= in.nextLine();
-        System.out.print("Last Name: ");
-        contact.lastName= in.nextLine();
-        System.out.print("Address: ");
-        contact.address= in.nextLine();
-        System.out.print("City: ");
-        contact.city= in.nextLine();
-        System.out.print("State: ");
-        contact.state= in.nextLine();
-        System.out.print("ZIP Code: ");
-        contact.zipCode= in.nextLine();
-        System.out.print("Phone Number: ");
-        contact.phoneNumber= in.nextLine();
-        System.out.print("E-mail Address: ");
-        contact.email= in.nextLine();
+        Contact[] contact = new Contact[100];
+        Scanner in = new Scanner(System.in);
 
-        System.out.println("____________________________________________________");
-        System.out.println("The Contact Details:");
-        System.out.println("Name: "+contact.firstName+" "+contact.lastName);
-        System.out.println("Address: "+contact.address+"\n"+contact.city+", "+contact.state+" - "+ contact.zipCode);
-        System.out.println("Phone Number: "+contact.phoneNumber+"\nE-mail Address: "+contact.address);
+        for (int i = 0; i < contact.length; i++) {
+            System.out.println("********************************************************************");
+            System.out.println("Do you want to ADD a new Contact to the Address Book?");
+            System.out.println("If YES: Press 1 else Press 2");
+            int input = in.nextInt();
+
+            switch (input) {
+                case 1:
+                    contact[i] = new Contact();
+                    contact[i].DisplayContact();
+                    break;
+                default:
+                    i = contact.length;
+            }
+        }
+    }
+
+    public static class Contact {
+        public String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
+
+        Contact() {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the following details to add the Contact to the Address Book:");
+            System.out.print("First Name: ");
+            firstName = in.nextLine();
+            System.out.print("Last Name: ");
+            lastName = in.nextLine();
+            System.out.print("Address: ");
+            address = in.nextLine();
+            System.out.print("City: ");
+            city = in.nextLine();
+            System.out.print("State: ");
+            state = in.nextLine();
+            System.out.print("ZIP Code: ");
+            zipCode = in.nextLine();
+            System.out.print("Phone Number: ");
+            phoneNumber = in.nextLine();
+            System.out.print("E-mail Address: ");
+            email = in.nextLine();
+        }
+
+        public void DisplayContact() {
+            System.out.println("____________________________________________________");
+            System.out.println("The Contact Details:");
+            System.out.println("Name: " + firstName + " " + lastName);
+            System.out.println("Address: " + address + "\n" + city + ", " + state + " - " + zipCode);
+            System.out.println("Phone Number: " + phoneNumber + "\nE-mail Address: " + address);
+        }
     }
 }
+
+
